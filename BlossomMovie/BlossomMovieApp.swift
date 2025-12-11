4ab01760195b6431e865ac1e4975cc6575109f20
Luiz Carlos Rocha Correa <luiz.correa.dev@gmail.com>
2025-12-11 14:01:00 +0000

    Initial Commit

---
+++ BlossomMovie/BlossomMovieApp.swift
@@ -0,0 +1,17 @@
+//
+//  BlossomMovieApp.swift
+//  BlossomMovie
+//
+//  Created by Luiz Correa on 11/12/2025.
+//
+
+import SwiftUI
+
+@main
+struct BlossomMovieApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
